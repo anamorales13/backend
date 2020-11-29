f478eea309f8f1bc3e01b4ebba5dd4d9c54b9fc3
anamorales13 <maldonado.morales@alu.uhu.es>
2020-11-29 18:47:23 +0100

    cambios

--- libs/storage.js
+++ libs/storage.js
@@ -3,11 +3,14 @@ var multer=require('multer');
 
 
 const storage= multer.diskStorage({
+    
     destination: function (req, file, cb){
         cb(null, './upload/users')
+        console.log("hola storage")
     },
     filename: function (req, file, cb){
         cb(null, `${file.originalname}-${Date.now()}`)
+        console.log("original" + file.originalname);
     }
 })
 
