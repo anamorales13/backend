8773bd65fc8190ae8cb4972045299d736cc6b438
anamorales13 <maldonado.morales@alu.uhu.es>
2020-11-30 11:15:42 +0100

    chat

--- index.js
+++ index.js
@@ -29,3 +29,54 @@ mongoose.connect(url,{ useUnifiedTopology: true, useNewUrlParser: true})
         });
 });
 
+
+//CHAT 
+
+const server = http.createServer(app);//creando el server con http y express como handle request
+const client = socketio(server);
+
+const {addUser, removeUser, getUser, getUserInRoom} =require('./controllers/user');
+
+
+server.listen(port, () => {
+    console.log("Server running in http://localhost:"+port)
+  })
+
+  
+
+client.on('connection', (socket) => {
+   
+    socket.on('join', ({name, room}, callback)=>{
+        const {error, user}= addUser({id:socket.id, name, room});
+        
+        if(error) return callback (error);
+        
+        socket.emit('message', {user:'admin', text:`${user.name}, Bienvenido a la sala ${user.room}`});
+        socket.broadcast.to(user.room).emit('message',{user:'admin', text:`${user.name}, se acaba de unir!`});
+
+        socket.join(user.room);
+
+        client.to(user.room).emit('roomData', {room: user.room, users: getUserInRoom(user.room)})
+
+        callback();
+    });
+
+    socket.on('sendMessage', (message, callback)=>{
+      
+        const user=getUser(socket.id);
+      
+        client.to(user.room).emit('message', {user:user.name, text:message});
+        client.to(user.room).emit('roomData', {room: user.room, users: getUserInRoom(user.room)});
+        callback();
+    });
+
+
+    socket.on('disconnect', () => {
+       const user= removeUser(socket.id);
+
+       if(user){
+           client.to(user.room).emit('message', {user:'admin', text:`${user.name} ha abandonado la sala.`})
+       }
+    })
+    
+});
