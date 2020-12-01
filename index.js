dbee6faf1136bdcb4d8085f148d076fff79a0a3e
anamorales13 <maldonado.morales@alu.uhu.es>
2020-12-01 19:56:03 +0100

    vuelta atrás con el chat

--- index.js
+++ index.js
@@ -46,15 +46,8 @@ server.listen(port, () => {
   })
 
   
+
   
-  server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-});
 
 client.on('connection', (socket) => {
    
